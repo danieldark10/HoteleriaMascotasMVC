33561ea83d3c20d3773e1d6f6f7468942319bf36
HP <Danielqarias97@outlook.com>
2022-07-22 23:44:24 -0400

    Actualizandon

---
+++ HoteleriaMascotas/Controllers/MascotaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoteleriaMascotas.Controllers
+{
+    public class MascotaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
