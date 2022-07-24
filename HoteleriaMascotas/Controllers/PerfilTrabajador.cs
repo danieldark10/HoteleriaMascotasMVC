ad1cd897359f394d27f92de57ff716d70f8725c0
HP <Danielqarias97@outlook.com>
2022-07-24 10:20:13 -0400

    sdkssk

---
+++ HoteleriaMascotas/Controllers/PerfilTrabajador.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoteleriaMascotas.Controllers
+{
+    public class PerfilTrabajador : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
