8700eae4ed8d232bc3c8849d57ae3fa20a4deef5
HP <Danielqarias97@outlook.com>
2022-07-29 02:18:10 -0400

    qqqqqq

--- HoteleriaMascotas/Controllers/MascotaController.cs
+++ HoteleriaMascotas/Controllers/MascotaController.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using HoteleriaMascotas.Models;
+using HoteleriaMascotas.Models.DB;
 namespace HoteleriaMascotas.Controllers
 {
     public class MascotaController : Controller
     {
-        public IActionResult Index()
+        
+        
+        public ActionResult Index (MascotaM Models)
         {
-            return View();
+            try
+            {
+             if (ModelState.IsValid)
+                {
+                    using (var db = new Models.DB.HoteleriaMascotasContext())
+                    {
+                        var Tabla = new Mascotum();
+                        Tabla.Idcliente = Models.idCliente;
+                        Tabla.Nombre = Models.Nombre;
+                        Tabla.Edad = Models.Edad;
+                        Tabla.Chip = Models.Chip;
+                        Tabla.Raza = Models.Raza;
+                        Tabla.Tipoanimal = Models.TipoDeAnimal;
+                        Tabla.Personalidad = Models.Personalidad;
+
+                        db.Mascota.Add(Tabla);
+                        db.SaveChanges();
+
+                    }
+                    return Redirect("/");
+                }
+                return View(Models);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
---
+++ HoteleriaMascotas/Models/MascotaM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HoteleriaMascotas.Models
+{
+    public class MascotaM
+    {
+        public int idMascota { get; set; }
+
+        [Required]
+        [Range(1, 60,ErrorMessage ="ingrese numeros desde 1 a 60")]
+        [Display(Name = "idCliente")]
+        public int idCliente { get; set; }
+
+       
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "Nombre")]
+        public String Nombre { get; set; }
+        
+        [Required]
+        [Range(25, 60, ErrorMessage = "Age must be between 25 and 60")]
+        [Display(Name = "Edad")]
+        public float Edad { get; set; }
+
+        
+        [Required]
+        [Range(1, 60, ErrorMessage = "ingrese numeros desde 1 a 60")]
+        [Display(Name = "Chip")]
+        public int Chip { get; set; }
+        
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "Raza")]
+        public String Raza { get; set; }
+        
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "TipoDeAnimal")]
+        public String TipoDeAnimal { get; set; }
+
+        
+        [Required]
+        [StringLength(15)]
+        [Display(Name = "Personalidad")]
+        public String Personalidad { get; set; }
+    }
+}
+
