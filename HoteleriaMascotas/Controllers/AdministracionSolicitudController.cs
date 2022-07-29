fe2f2ae3a5d0444e65dcc3ba31baa3b2d8d86d2d
HP <Danielqarias97@outlook.com>
2022-07-29 18:39:01 -0400

    dsff

---
+++ HoteleriaMascotas/Controllers/AdministracionSolicitudController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoteleriaMascotas.Controllers
+{
+    public class AdministracionSolicitudController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HoteleriaMascotas/Controllers/Pagos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoteleriaMascotas.Controllers
+{
+    public class Pagos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
