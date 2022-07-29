939346dd7a5c0c7aa6cd52e1cd0bfe41c68e8d66
HP <Danielqarias97@outlook.com>
2022-07-29 17:04:28 -0400

    xdcdsc

---
+++ HoteleriaMascotas/Controllers/Habitacion.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoteleriaMascotas.Controllers
+{
+    public class Habitacion : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HoteleriaMascotas/Controllers/Tipodesolicitudcotroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoteleriaMascotas.Controllers
+{
+    public class Tipodesolicitudcotroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
